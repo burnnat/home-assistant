614fbce4f9dfc9b4368b7d2f2e5a723e078d3aa6
Nat Burns <nbaccount@burnskids.com>
2019-05-30 12:17:53 +0000

    Add support for asset aliases.

--- homeassistant/components/screenly/media_player.py
+++ homeassistant/components/screenly/media_player.py
@@ -18,15 +18,20 @@ _LOGGER = logging.getLogger(__name__)
 
 DATA_SCREENLY = 'screenly'
 DEFAULT_NAME = 'Screenly'
+CONF_ASSETS = 'assets'
 
-SUPPORT_SCREENLY = SUPPORT_PLAY_MEDIA | SUPPORT_NEXT_TRACK | SUPPORT_PREVIOUS_TRACK
+SUPPORT_SCREENLY = (
+    SUPPORT_PLAY_MEDIA | SUPPORT_NEXT_TRACK | SUPPORT_PREVIOUS_TRACK)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_PORT, default=80): cv.port,
     vol.Optional(CONF_SSL, default=False): cv.boolean,
-    vol.Optional(CONF_TIMEOUT, default=5): cv.positive_int
+    vol.Optional(CONF_TIMEOUT, default=5): cv.positive_int,
+    vol.Optional(CONF_ASSETS): {
+        cv.string: cv.string
+    }
 })
 
 
@@ -41,9 +46,10 @@ async def async_setup_platform(
     port = config.get(CONF_PORT)
     ssl = config.get(CONF_SSL)
     timeout = config.get(CONF_TIMEOUT)
+    assets = config.get(CONF_ASSETS)
 
     screenly = ScreenlyDevice(async_get_clientsession(
-        hass, verify_ssl=False), name, host, port, ssl, timeout)
+        hass, verify_ssl=False), name, host, port, ssl, timeout, assets)
 
     hass.data[DATA_SCREENLY].append(screenly)
     async_add_entities([screenly], update_before_add=True)
@@ -52,12 +58,14 @@ async def async_setup_platform(
 class ScreenlyDevice(MediaPlayerDevice):
     """Representation of Screenly digital signage device."""
 
-    def __init__(self, websession, name, host, port, encryption, timeout):
+    def __init__(
+            self, websession, name, host, port, encryption, timeout, assets):
         """Initialize entity to control Screenly device."""
         import aiohttp
         import screenly_ose as screenly
 
         self._name = name
+        self._assets = assets
         self._state = None
 
         client_timeout = aiohttp.ClientTimeout(total=timeout)
@@ -116,7 +124,14 @@ class ScreenlyDevice(MediaPlayerDevice):
     async def async_play_media(self, media_type, media_id, **kwargs):
         """Switch asset to given ID."""
         if media_type in [MEDIA_TYPE_IMAGE, MEDIA_TYPE_VIDEO, MEDIA_TYPE_URL]:
-            response = await self._screenly.switch_asset(media_id)
+            if media_id in self._assets:
+                asset_id = self._assets[media_id]
+                _LOGGER.debug("Found matching id '%s' for alias '%s'",
+                    asset_id, media_id)
+            else:
+                asset_id = media_id
+
+            response = await self._screenly.switch_asset(asset_id)
             return bool(response)
         else:
             _LOGGER.error(
